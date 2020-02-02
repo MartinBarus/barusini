04fbc183cb40f782c14d689290009c68791eed5f
Martin Barus <martin.barus@gmail.com>
2020-02-02 21:27:20 +0100

    basic hyperparameter tunning

--- barusini/features.py
+++ barusini/features.py
@@ -15,7 +15,6 @@ from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from tqdm import tqdm as tqdm
 from joblib import Parallel, delayed
-from lightgbm import LGBMClassifier
 from xgboost import XGBClassifier
 
 from barusini.transformers.transformer import Pipeline
@@ -25,18 +24,17 @@ from barusini.transformers.encoders import (
     CustomLabelEncoder,
     TargetEncoder,
 )
+from barusini.model_tuning import optimize_xboost
 from barusini.utils import (
-    format_time,
     get_terminal_size,
-    load_object,
     save_object,
+    load_object,
+    format_time,
 )
 
 ESTIMATOR = XGBClassifier(seed=42)
-# ESTIMATOR = LGBMClassifier(random_state=42)
-# ESTIMATOR = RandomForestClassifier(n_estimators=100, n_jobs=-1,
-# random_state=42)
-CV = StratifiedKFold(n_splits=3, random_state=42)
+# ESTIMATOR = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=42)
+CV = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
 SCORER = roc_auc_score
 MAXIMIZE = True
 STAGE_NAME = "STAGE"
@@ -309,13 +307,25 @@ def feature_engineering(X, y, model_path, **kwargs):
     return model
 
 
-def model_search(X, y, model, model_path):
-    return model
+def model_search(X_train, y_train, model, X_test, model_path):
+    best = optimize_xboost(
+        model, X_train, y_train, X_test, CV, SCORER, MAXIMIZE
+    )
+    new_model = copy.deepcopy(model)
+    print("BEST PARAMS", best.params)
+    new_model.model = XGBClassifier(**best.params)
+    print(new_model)
+    new_model.fit(X_train, y_train)
+    if model_path:
+        print("Saving model to", model_path)
+        save_object(model, model_path)
+    return new_model
 
 
 def auto_ml(X, y, model_path=None, **kwargs):
     model = feature_engineering(X, y, model_path=model_path)
-    model = model_search(X, y, model, model_path)
+    model = model_search(X, y, model, model_path, None, model_path)
+    return model
 
 
 def predict(X, model, included_columns, output_path, probability):
@@ -371,6 +381,20 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--features",
+        action="store_true",
+        default=False,
+        help="Find best features",
+    )
+
+    parser.add_argument(
+        "--tune",
+        action="store_true",
+        default=False,
+        help="Optimize final model",
+    )
+
+    parser.add_argument(
         "--drop", metavar="N", type=str, nargs="+", help="drop original columns"
     )
 
@@ -395,13 +419,18 @@ if __name__ == "__main__":
     if args.predict or args.predict_proba:
         transformers = load_object(model_file)
         predict(X, transformers, args.include, args.output, args.predict_proba)
-    else:
+    if args.features:
         y = X[target]
         dropped = args.drop
         dropped = dropped if dropped else []
         dropped = dropped + [target]
         X = X.drop(dropped, axis=1)
-        X_ = auto_ml(X, y, model_path=model_file)
+        X_ = feature_engineering(X, y, model_path=model_file)
+    if args.tune:
+        if not args.features:
+            y = X[target]
+        model = load_object(model_file)
+        model = model_search(X, y, model, None, model_file)
 
     duration = format_time(time.time() - start)
     print(f"Duration: {duration}")
---
+++ barusini/model_tuning.py
@@ -0,0 +1,90 @@
+import optuna
+import copy
+import numpy as np
+from xgboost import XGBClassifier
+from functools import partial
+
+
+def xgboost_objective(
+    model, X_train, y_train, X_test, cv, scoring, maximize, trial,
+):
+    min_child_weight = trial.suggest_loguniform("min_child_weight", 1e-1, 1e3)
+    # gamma = [0.5, 1, 1.5, 2, 5]
+    subsample = trial.suggest_uniform("subsample", 0.6, 1)
+    colsample_bytree = trial.suggest_uniform("colsample_bytree", 0.6, 1)
+    max_depth = trial.suggest_int("max_depth", 3, 12)
+
+    params = dict(
+        min_child_weight=min_child_weight,
+        subsample=subsample,
+        colsample_bytree=colsample_bytree,
+        max_depth=max_depth,
+        learning_rate=trial.suggest_categorical('learning_rate', [0.01]),
+        seed=trial.suggest_categorical('seed', [42]),
+        n_estimators=1000,
+    )
+
+    new_model = copy.deepcopy(model)
+    new_model.model = XGBClassifier(**params)
+
+    oof = np.zeros(len(X_train))
+    preds = None
+    if X_test is not None:
+        preds = np.zeros(len(X_test))
+
+    n_iterations = []
+    for i, (idxT, idxV) in enumerate(cv.split(X_train, y_train)):
+
+        print(" rows of train =", len(idxT), "rows of holdout =", len(idxV))
+        X_act_train = X_train.iloc[idxT]
+        y_act_train = y_train.iloc[idxT]
+
+        X_act_val = X_train.iloc[idxV]
+        y_act_val = y_train.iloc[idxV]
+
+        new_model.fit(
+            X_act_train,
+            y_act_train,
+            eval_set=[(X_act_val, y_act_val)],
+            verbose=0,
+            early_stopping_rounds=20,
+        )
+
+        oof[idxV] += new_model.predict_proba(X_act_val)[:, 1]
+        if X_test is not None:
+            preds += new_model.predict_proba(X_test)[:, 1] / cv.n_splits
+
+        n_iterations.append(new_model.model.best_iteration)
+
+
+    iter_mean = np.mean(n_iterations)
+    iter_std = np.std(n_iterations)
+    n_estimators = int(np.round(iter_mean))
+    trial.suggest_int('n_estimators', n_estimators, n_estimators)
+    score = scoring(y_train, oof)
+    score = -score if maximize else maximize
+    print("XGB OOF CV=", score, "mean", iter_mean, "std", iter_std)
+    return score
+
+
+def optimize_xboost(
+    model, X_train, y_train, X_test, cv, scoring, maximize, n_trials=20
+):
+    objective = partial(
+        xgboost_objective,
+        model,
+        X_train,
+        y_train,
+        X_test,
+        cv,
+        scoring,
+        maximize,
+    )
+
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=n_trials)
+    print(
+        f"Out of {n_trials} trials the best score is {study.best_value}"
+        f" with params {study.best_params}"
+    )
+    return study.best_trial
