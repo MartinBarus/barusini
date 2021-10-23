db1a92b98c4ca13e210996cbc7d5ee09bab5d335
Martin Barus <martin.barus@gmail.com>
2021-10-23 10:38:33 +0200

    add requirements install

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import pip
 import setuptools
+
+versions = tuple(int(x) for x in pip.__version__.split("."))
+if versions >= (10, 0, 0):
+    from pip._internal.req import parse_requirements
+else:
+    from pip.req import parse_requirements
+
+requirements = parse_requirements("requirements.txt", session="requirements")
+requirements = [x.requirement for x in requirements]
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -12,15 +23,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MartinBarus/barusini",
-    project_urls={
-        "Bug Tracker": "https://github.com/MartinBarus/barusini/issues",
-    },
+    project_urls={"Bug Tracker": "https://github.com/MartinBarus/barusini/issues"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "barusini"},
-    packages=setuptools.find_packages(where="barusini"),
+    install_requires=requirements,
+    packages=setuptools.find_packages(include=["barusini"]),
     python_requires=">=3.6",
 )
