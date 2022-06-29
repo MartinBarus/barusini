4989e25fe2f69507ef8313fe2e2c69ee14cc7489
Martin Barus <martin.barus@gmail.com>
2022-06-29 15:29:40 +0200

    add linear head

--- barusini/nn/image/low_level_model.py
+++ barusini/nn/image/low_level_model.py
@@ -6,6 +6,10 @@ from barusini.nn.generic.loading import Serializable
 from barusini.nn.generic.utils import get_real_n_classes
 from torch import nn
 
+SIMPLE_HEAD = "simple"
+LINEAR_HEAD = "linear"
+HEADS = [SIMPLE_HEAD, LINEAR_HEAD]
+
 
 class ImageNet(nn.Module, Serializable):
     def __init__(
@@ -15,29 +19,48 @@ class ImageNet(nn.Module, Serializable):
         in_channels=3,
         pretrained_weights=True,
         classification=None,
-        **kwargs
+        head=LINEAR_HEAD,
+        **kwargs,
     ):
         super().__init__()
 
         if classification:
             n_classes = get_real_n_classes(n_classes)
 
+        if head == SIMPLE_HEAD:
+            timm_n_classes = n_classes
+        else:
+            timm_n_classes = 0
+
         self.net = timm.create_model(
             model_name=backbone,
             pretrained=pretrained_weights,
-            num_classes=n_classes,
+            num_classes=timm_n_classes,
             drop_rate=0,
             in_chans=in_channels,
         )
 
+        if head == LINEAR_HEAD:
+            self.head = torch.nn.Linear(self.net.num_features, n_classes)
+        elif head == SIMPLE_HEAD:
+            self.head = self._simple_head
+        else:
+            raise ValueError(f"'{head}' is not supported head. Choose one of {HEADS}.")
+
     def forward(self, input_dict, mode):
-        return {"logits": self.net(input_dict)}
+        x = self.net(input_dict)
+        x = self.head(x)
+        return {"logits": x}
 
     def load_weights(self, pretrained_weights):
         state_dict = torch.load(pretrained_weights, map_location="cpu")
         state_dict = {k[6:]: v for k, v in state_dict["state_dict"].items()}
         self.load_state_dict(state_dict, strict=True)
         print("weights loaded from", pretrained_weights)
+
+    @staticmethod
+    def _simple_head(x):
+        return x
 
     @classmethod
     def from_folder(
